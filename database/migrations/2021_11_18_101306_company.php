9ef156e603b054235a3feee9c407c2cd4be488ea
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2021-11-29 17:32:46 +0300

    fixed some bugs on database names

--- database/migrations/2021_11_18_101306_company.php
+++ database/migrations/2021_11_18_101306_company.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class GameCompany extends Migration
+class Company extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2021_11_26_131917_comment_news.php
+++ database/migrations/2021_11_26_131917_comment_news.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CommentNew extends Migration
+class CommentNews extends Migration
 {
     /**
      * Run the migrations.
