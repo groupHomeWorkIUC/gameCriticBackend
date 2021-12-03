f25143f69cf9f0bee7f102f0c940d56d52a35c77
İbrahim Yavuz <ibrahim.yavuz.0665@gmail.com>
2021-12-03 14:56:35 +0300

    expiring time has been set to infinite

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,12 +12,17 @@ class AuthController extends Controller
 
     public function __construct()
     {
-       // $this->middleware('auth:api', ['except' => ['login']]);
+       $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function index(){
+
     }
 
     public function loginPage(){
         return "Giriş yapınız";
     }
+
     public function login()
     {
         $credentials = request(['email', 'password']);
